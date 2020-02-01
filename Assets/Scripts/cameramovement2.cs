41cb974884c9645212aceb3e4118f78efb00627f
Wayne <wayne980224@outlook.com>
2020-02-01 14:47:13 -0800

    123
    3123

--- Assets/Scripts/cameramovement2.cs
+++ Assets/Scripts/cameramovement2.cs
@@ -2,18 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class cameramovement : MonoBehaviour
+public class cameramovement2 : MonoBehaviour
 {
+    // Start is called before the first frame update
     public Transform target;
+    private bool updateornot = true;
 
-    [SerializeField]public float smoothSpeed;
+    [SerializeField] public float smoothSpeed;
 
     [SerializeField] public Vector3 Offset;
 
     void FixedUpdate()
     {
-        if (camerasystem.instance.updateornot)
-        {
+        if (updateornot)
+         {
             Vector3 desiredPosition = target.position + Offset;
             Vector3 smoothedPosition = Vector3.Lerp(this.transform.position, desiredPosition, smoothSpeed);
             transform.position = smoothedPosition;
---
+++ Assets/Scripts/camerasystem.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camerasystem : MonoBehaviour
+{
+    static public camerasystem instance;
+    public GameObject Cam1;
+    public GameObject Cam2;
+    private Animator a;
+    public bool updateornot = true;
+    // Start is called before the first frame update
+
+    private void Awake()
+    {
+        instance = this;
+    }
+    void Start()
+    {
+        a = this.GetComponent<Animator>();
+        a.SetBool("change", false);
+    }
+
+    public void MergeCamera()
+    {
+        updateornot = false;
+        a.SetBool("change", true);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(Vector3.Distance(Cam1.transform.position, Cam2.transform.position));
+        if ( Vector3.Distance(Cam1.transform.position, Cam2.transform.position) < 5)
+        {
+            MergeCamera();
+        }
+    }
+}

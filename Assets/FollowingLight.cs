929566863c78d2e622b89011b413ea8fffb8b1a6
TIANCHENG <306489696@QQ.COM>
2020-02-01 13:37:16 -0800

    light model

---
+++ Assets/FollowingLight.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowingLight : MonoBehaviour
+{
+    public Transform targetPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position = targetPosition.position + new Vector3(0.294f, 0.294f, 0.333f);
+    }
+}

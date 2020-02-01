d971c21dfa136e5e519af6404d4df70aacbabddc
TIANCHENG <306489696@QQ.COM>
2020-02-01 11:58:00 -0800

    movement implemented

---
+++ Assets/Scripts/FixedFace.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FixedFace : MonoBehaviour
+{
+    private Transform body;
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = this.GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        body.rotation = Quaternion.identity;
+    }
+}
---
+++ Assets/Scripts/MovePlan1.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlan1 : MonoBehaviour
+{
+    private Rigidbody rb;
+    private Vector3 movement;
+    public float speed = 5f;
+    public float gravityCof = 9.8f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")) + Vector3.up * Physics.gravity.y * Time.deltaTime * gravityCof;
+
+    }
+
+    void FixedUpdate()
+    {
+        Move(movement);
+    }
+
+    void Move(Vector3 mm)
+    {
+        rb.velocity = mm * speed;
+    }
+
+}

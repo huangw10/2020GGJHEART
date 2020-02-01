5110fa46f40ffd26aab78a266d7cdfe3199f5d54
Wayne <wayne980224@outlook.com>
2020-02-01 13:01:49 -0800

    123
    3131313

---
+++ Assets/Scripts/cameramovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameramovement : MonoBehaviour
+{
+    public Transform target;
+
+    [SerializeField]public float smoothSpeed;
+
+    [SerializeField] public Vector3 Offset;
+
+    void FixedUpdate()
+    {
+        Vector3 desiredPosition = target.position + Offset;
+        Vector3 smoothedPosition = Vector3.Lerp(this.transform.position, desiredPosition, smoothSpeed);
+        transform.position = smoothedPosition;
+
+        transform.LookAt(target);
+    }
+}

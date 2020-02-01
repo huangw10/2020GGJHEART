d114f6d83204d3ab743cfdc2c15cf57c515c4a50
Wayne <wayne980224@outlook.com>
2020-02-01 15:16:11 -0800

    123
    312313

--- Assets/Scripts/camerasystem.cs
+++ Assets/Scripts/camerasystem.cs
@@ -9,7 +9,7 @@ public class camerasystem : MonoBehaviour
     public GameObject Cam1;
     public GameObject Cam2;
     private Animator a;
-    [SerializeField] float distance;
+    [SerializeField] private float distance;
     public bool updateornot = true;
     // Start is called before the first frame update
 
@@ -28,18 +28,32 @@ public class camerasystem : MonoBehaviour
         {
             updateornot = false;
             a.SetTrigger("change1");
-            MainCamera.transform.rotation = Cam1.transform.rotation;
-            MainCamera.transform.position = Cam1.transform.position;
         }
         
+    }
+
+    public void SplitCamera()
+    {
+        Debug.Log("split");
+        if (updateornot == false)
+        {
+            Debug.Log("split");
+            updateornot = true;
+            a.SetTrigger("change2");
+        }
+
     }
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(Vector3.Distance(Cam1.transform.position, Cam2.transform.position));
-        if ( Vector3.Distance(Cam1.transform.position, Cam2.transform.position) < distance)
+        if (updateornot && Vector3.Distance(Cam1.transform.position, Cam2.transform.position) < distance)
         {
             MergeCamera();
+        }
+        if (updateornot == false && Vector3.Distance(Cam1.transform.position, Cam2.transform.position) >= distance)
+        {
+            Debug.Log("split");
+            SplitCamera();
         }
     }
 }

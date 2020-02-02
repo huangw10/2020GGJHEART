cced1d43be0ac06fbdf49bd545e070ea48862b08
TIANCHENG <306489696@QQ.COM>
2020-02-02 11:53:00 -0800

    navig function implemented

---
+++ Assets/Scripts/Hint.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hint : MonoBehaviour
+{
+    public Transform my_position;
+    public Transform target_player;
+    private Transform mp;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mp = this.GetComponent<Transform>();
+        this.GetComponentInChildren<ParticleSystem>().Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mp.position = my_position.position;
+
+
+        Vector3 direction = target_player.position - my_position.position;
+        Quaternion rotation = Quaternion.LookRotation(direction);
+        mp.rotation = rotation;
+        mp.eulerAngles = new Vector3(90, mp.eulerAngles.y, mp.eulerAngles.z + 65);
+        Debug.Log("mp.rotation");
+        // mp.Rotate();
+
+    }
+}

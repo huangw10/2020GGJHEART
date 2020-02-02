22a34cca76c7f4ca1f987c1ddbfc4082d9056b8c
wizardkingrabbit <jingtil2@uci.edu>
2020-02-02 10:23:42 +0800

    一些场景道具
    可开关的空气墙
    物品定点生成器
    空气墙之消灭者

---
+++ Assets/Scripts/lifereducer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lifereducer : MonoBehaviour
+{
+    public List<airwall> reduceit;
+    private int count = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        for (count = 0; count < 2; count++)
+        {
+            reduceit[count].reducelife();
+        }
+    }
+}

840e3227fa78d2f1f912f58ac1d1c29afbc6778e
TIANCHENG <306489696@QQ.COM>
2020-02-05 15:01:19 -0800

    added exit object

---
+++ Assets/Scripts/Exit.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Exit : MonoBehaviour
+{
+    public GameObject title;
+
+
+
+    void Start()
+    {
+        title.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray ray = this.GetComponent<Camera>().ScreenPointToRay(Input.mousePosition);
+        
+        RaycastHit hit;
+
+        if (title.activeSelf)
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                title.SetActive(false);
+            }
+        }
+        else
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                title.SetActive(true);
+            }
+        }
+
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            if (hit.transform.tag == "Yes")
+            {
+                Debug.Log("Exit");
+            }
+        }
+    }
+
+    void Close()
+    {
+        Application.Quit();
+    }
+}

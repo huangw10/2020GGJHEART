868e0ebebcd1db9dc8a19e4fd5ee7f0a1938268a
TIANCHENG <306489696@QQ.COM>
2020-02-05 16:53:55 -0800

    Exit menu added

--- Assets/Scripts/Exit.cs
+++ Assets/Scripts/Exit.cs
@@ -5,8 +5,11 @@ using UnityEngine;
 public class Exit : MonoBehaviour
 {
     public GameObject title;
-
-
+    public Material red;
+    public Material blue;
+    public GameObject rightYes1;
+    public GameObject rightYes2;
+    public float camera_num = 0;
 
     void Start()
     {
@@ -16,37 +19,69 @@ public class Exit : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Ray ray = this.GetComponent<Camera>().ScreenPointToRay(Input.mousePosition);
-        
-        RaycastHit hit;
 
-        if (title.activeSelf)
-        {
-            if (Input.GetKeyDown(KeyCode.Escape))
-            {
-                title.SetActive(false);
-            }
-        }
-        else
-        {
-            if (Input.GetKeyDown(KeyCode.Escape))
+            if (UImanager.instance.isOpen)
             {
                 title.SetActive(true);
             }
+            else
+            {
+                title.SetActive(false);
+            }
+
+
+
+
+        if (camera_num == 0)
+        {
+            if (title.activeSelf)
+            {
+                if (Input.GetKeyDown(KeyCode.Escape))
+                {
+                    UImanager.instance.isOpen = false;
+                }
+            }
+            else
+            {
+                if (Input.GetKeyDown(KeyCode.Escape))
+                {
+                    UImanager.instance.isOpen = true;
+                }
+            }
         }
 
-
+        Ray ray = this.GetComponent<Camera>().ScreenPointToRay(Input.mousePosition);
+        
+        RaycastHit hit;
+    
         if (Physics.Raycast(ray, out hit))
         {
             if (hit.transform.tag == "Yes")
             {
-                Debug.Log("Exit");
+                rightYes1.GetComponent<Renderer>().material = red;
+                rightYes2.GetComponent<Renderer>().material = red;
+                if (Input.GetMouseButtonDown(0))
+                    Close();
+            }
+
+            else if (hit.transform.tag == "No")
+            {
+                rightYes1.GetComponent<Renderer>().material = red;
+                rightYes2.GetComponent<Renderer>().material = red;
+                if (Input.GetMouseButtonDown(0))
+                    UImanager.instance.isOpen = false;
+            }
+            else
+            {
+                rightYes1.GetComponent<Renderer>().material = blue;
+                rightYes2.GetComponent<Renderer>().material = blue;
             }
         }
     }
 
     void Close()
     {
+        Debug.Log("close");
         Application.Quit();
     }
 }
---
+++ Assets/Scripts/UImanager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UImanager : MonoBehaviour
+{
+    public static UImanager instance;
+    public bool isOpen = false;
+    // Start is called before the first frame update
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
+
+}
